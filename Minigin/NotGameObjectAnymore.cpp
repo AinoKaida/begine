2cde55eb932d209e6146fe37802669797a4ab612
Celine Vandewalle <celine.vandewalle@student.howest.be>
2020-08-05 20:20:40 +0200

    Deleted expired files

--- Minigin/NotGameObjectAnymore.cpp
+++
@@ -1,9 +0,0 @@
-#include "MiniginPCH.h"
-#include "NotGameObjectAnymore.h"
-#include "Scene.h"
-
-dae::NotGameObjectAnymore::NotGameObjectAnymore(unsigned int objectID, std::shared_ptr<dae::Scene> scene)
-	: m_ObjectID{ objectID }
-	, m_pScene{ scene }
-{
-}
